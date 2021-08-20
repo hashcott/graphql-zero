2050dcaf437a4689ccbd32040e138bf422cdb73e
fungerouscode <duchanhstyle@gmail.com>
2021-08-20 17:01:32 +0700

    :sparkles: Simple server with typeDefs and resolvers

--- graphql-basic/src/index.js
+++ graphql-basic/src/index.js
@@ -1,1 +1,38 @@
-console.log("Hello GraphQL");
+import { GraphQLServer } from "graphql-yoga";
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`;
+// Resolvers
+
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my first query!";
+    },
+    name() {
+      return "Harry";
+    },
+    location() {
+      return "Ha Noi";
+    },
+    bio() {
+      return 1;
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log("The server is up!");
+});

869b382767fa90101ad5fde38e56ba0a20fd3059
fungerouscode <duchanhstyle@gmail.com>
2021-08-21 15:37:15 +0700

    :art: challenge with scalar type

--- graphql-basic/src/index.js
+++ graphql-basic/src/index.js
@@ -3,31 +3,31 @@ import { GraphQLServer } from "graphql-yoga";
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        id : ID!
-        name: String!
-        age: Int!
-        employed: Boolean
-        gpa: Float
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!
     }
 `;
 // Resolvers
 
 const resolvers = {
   Query: {
-    id() {
-      return "abc123";
+    title() {
+      return "Brixon 150";
     },
-    name() {
-      return "Harry";
+    price() {
+      return 56000000.0;
     },
-    age() {
-      return 21;
+    releaseYear() {
+      return 2021;
     },
-    employed() {
+    rating() {
+      return 4.9;
+    },
+    inStock() {
       return true;
-    },
-    gpa() {
-      return 3.6;
     },
   },
 };

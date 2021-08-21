bc178451cff521c96fc870f95ec0b970a01f9c44
fungerouscode <duchanhstyle@gmail.com>
2021-08-21 16:19:37 +0700

    :sparkles: Working with Array Part 1

--- graphql-basic/src/index.js
+++ graphql-basic/src/index.js
@@ -4,7 +4,8 @@ import { GraphQLServer } from "graphql-yoga";
 const typeDefs = `
     type Query {
         greeting(name: String!, position: String!): String!
-        add(a: Int!, b: Int!) : Int!
+        add(numbers: [Int]!) : Int!
+        grades: [Int]!
         user: User!
         post: Post!
     }
@@ -32,7 +33,15 @@ const resolvers = {
       return "Hello !";
     },
     add(parent, args, ctx, info) {
-      return args.a + args.b;
+      if (args.numbers.length === 0) {
+        return 0;
+      }
+      return args.numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      });
+    },
+    grades(parent, args, ctx, info) {
+      return [20, 21, 22];
     },
     user() {
       return {
